734a058f043319fa5656269db53c64b8a8ee9727
RaM <0wines.pumpkin@icloud.com>
2023-01-17 01:23:15 +0900

    navbar js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-'use strict'
+'use strict';
+
+// Make navbar transparent top and color when scrolling;
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll',() => {
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark');
+    }else{
+        navbar.classList.remove('navbar--dark');
+    }
+})
