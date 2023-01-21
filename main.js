af9efcd75ec5cd64bc38b61b283cf86ff610ed1d
RaM <0wines.pumpkin@icloud.com>
2023-01-21 14:16:54 +0900

    project filtering

--- main.js
+++ main.js
@@ -51,6 +51,28 @@ arrowUp.addEventListener('click',() =>{
     scrollIntoViews('#home');
 })
 
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    projectContainer.classList.add('anime-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anime-out');
+    },300);
+});
+
 function scrollIntoViews(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'}); //behavior is not working on safari
